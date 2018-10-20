fa4a3be871694879c3aced3b77747e081832802f
farziengineer <anand.mohit001@gmail.com>
2018-10-20 23:20:40 +0530

    Added final

--- src.main.java/com/nissan/AddController.java
+++ src.main.java/com/nissan/AddController.java
@@ -1,13 +1,26 @@
 package com.nissan;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AddController {
 	
 	@RequestMapping("/add")
-	public void add() {
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
+		
+		int i = Integer.parseInt(request.getParameter("num1"));
+		int j = Integer.parseInt(request.getParameter("num2"));
+		int sum = i + j;
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("display.jsp");
+		mv.addObject("result", sum);
 		System.out.println("Numbers added");
+		return mv;
 	}
 }
